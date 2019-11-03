857dcb7dfe6c7131f81a309f49d42cf75ae8a227
Viktor Dohanyos <vorkapp@gmail.com>
2019-11-03 19:19:32 +0100

    database working

--- src/main/java/hw/loan/controller/LoginController.java
+++ src/main/java/hw/loan/controller/LoginController.java
@@ -1,14 +1,32 @@
 package hw.loan.controller;
 
+import hw.loan.model.User;
+import hw.loan.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import java.util.List;
+
 @Controller
 public class LoginController {
-    @RequestMapping(value= {"login", "/"}, method = RequestMethod.GET)
-    public String view() {
 
+    @Autowired
+    private UserService us;
+
+    @RequestMapping(value= {"login", "/"}, method = RequestMethod.GET)
+    public String view(Model model) {
+
+        List<User> userList = us.getAllUsers();
+
+        for (User user : userList) {
+            System.out.printf("Id: %s -- Email: %s -- Permissions: %s ", user.getId(), user.getEmail(), user.getPermissions());
+            System.out.println("");
+        }
+
+        model.addAttribute("userList", userList);
         return "login";
     }
 }

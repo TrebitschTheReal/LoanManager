2bfa58e7400c6ec46b0418a35c3856a80de1c1bf
Viktor Dohanyos <vorkapp@gmail.com>
2019-11-04 22:42:05 +0100

    index page created

--- src/main/java/hw/loan/controller/LoginController.java
+++ src/main/java/hw/loan/controller/LoginController.java
@@ -1,24 +1,24 @@
-package hw.loan.controller;
-
-import hw.loan.model.User;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-
-import java.util.List;
-
-@Controller
-public class LoginController {
-
-    @RequestMapping(value= {"login", "/"}, method = RequestMethod.GET)
-    public String view(Model model) {
-
-//        List<User> userList = us.getAllUsers();
+//package hw.loan.controller;
 //
-//        model.addAttribute("userList", userList);
-
-        return "login";
-    }
-}
+//import hw.loan.model.User;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestMethod;
+//
+//import java.util.List;
+//
+//@Controller
+//public class LoginController {
+//
+//    @RequestMapping(value= {"login", "/"}, method = RequestMethod.GET)
+//    public String view(Model model) {
+//
+////        List<User> userList = us.getAllUsers();
+////
+////        model.addAttribute("userList", userList);
+//
+//        return "login";
+//    }
+//}

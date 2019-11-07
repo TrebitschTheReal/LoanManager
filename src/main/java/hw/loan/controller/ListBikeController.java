9e5cd2bb79afcce0bbefd216eca012dfe5a7d06f
Viktor Dohanyos <dohanyos.viktor@innostart.hu>
2019-11-07 17:23:22 +0100

    update

--- src/main/java/hw/loan/controller/ListBikeController.java
+++ src/main/java/hw/loan/controller/ListBikeController.java
@@ -5,6 +5,7 @@ import hw.loan.service.BikeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,14 +19,21 @@ public class ListBikeController {
     private BikeService bikeService;
 
     @RequestMapping(value={"rentbike", "managebike"}, method = RequestMethod.GET)
-    public String listAllBike(@RequestParam (required = false)String bikeParam, Model model) {
+    public String listAllBike(Model model) {
         List<Bike> bikeList = bikeService.listBike();
-        System.out.println(bikeParam);
-
-
-
-
         model.addAttribute("bikeList", bikeList);
+        return "index";
+    }
+
+    @RequestMapping(value={"rentbike", "managebike"}, params = {"modifiableBikeId"}, method = RequestMethod.POST)
+    public String modifyBike(@RequestParam("modifiableBikeId") String modifiableBikeId, Model model) {
+        List<Bike> bikeList = bikeService.listBike();
+        System.out.println(modifiableBikeId);
+
+
+
+
+       // model.addAttribute("bikeList", bikeList);
         return "index";
     }
 }
